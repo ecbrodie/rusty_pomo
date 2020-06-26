01cafaa7e982f9d7e80b5dd681e793a8d9572d4b
Evan Brodie <brodie.evan@gmail.com>
2020-06-26 17:10:28 -0400

    Uses crossterm to clear the line every second that passes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate chrono;
-use chrono::{DateTime, Duration, Utc}; // TODO: Why am I getting an IDE warning about Duration?
-use std::fmt::{Display, Formatter, Result};
+extern crate crossterm;
+
+use chrono::{DateTime, Duration, Utc};
+use std::io::{stdout, Stdout, Write};
+
+use crossterm::{cursor::*, terminal::*, ExecutableCommand, Result};
 
 const TIMER_IN_SECONDS: i64 = 5;
 
@@ -11,40 +15,49 @@ struct Timer {
 impl Timer {
     fn new() -> Timer {
         Timer {
-            // end_time: SystemTime::now().checked_add(Duration::from_secs(5)).expect("Desired timer end time is too far in the future."),
             end_time: Utc::now() + Duration::seconds(TIMER_IN_SECONDS),
         }
     }
 
     fn seconds_remaining(&self) -> i64 {
         let now = Utc::now();
-        (self.end_time - now).num_seconds() // TODO: Is subtraction here correct? Also, why does num_seconds() not autocomplete?
+        (self.end_time - now).num_seconds()
     }
 }
 
-impl Display for Timer {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "{}", self.end_time.timestamp())
-    }
+fn clear_line(stdout: &mut Stdout) -> Result<()> {
+    stdout
+        .execute(Clear(ClearType::CurrentLine))?
+        .execute(RestorePosition)?;
+
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let timer = Timer::new();
-    println!("Starting a timer that ends at {}", timer);
+    println!("Starting a timer that lasts {} seconds", TIMER_IN_SECONDS);
+
+    let mut stdout = stdout();
 
     let mut previous_seconds_remaining = TIMER_IN_SECONDS;
+    stdout.execute(SavePosition)?;
     loop {
         let seconds_remaining = timer.seconds_remaining();
 
         if seconds_remaining < previous_seconds_remaining {
-            println!("{} seconds remaining...", seconds_remaining);
+            clear_line(&mut stdout)?;
+            print!("{} seconds remaining...", seconds_remaining);
+            stdout.flush()?;
         }
         previous_seconds_remaining = seconds_remaining;
 
         if seconds_remaining <= 0 {
+            clear_line(&mut stdout)?;
             break;
         }
     }
 
     println!("DING DING DING! Times up.");
+
+    Ok(())
 }
